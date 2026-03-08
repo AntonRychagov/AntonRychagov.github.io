c19b4488783020647ddef055208be6601324284d
Anton Rychagov <anton.rychagov@users.noreply.github.com>
2026-03-09 00:13:50 +0300

    Sync site with latest basic-page-mate build

--- assets/projects-BJZFma7V.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./index-Cq_qOR_y.js";const s=e.projects,a=t=>s.find(r=>r.slug===t),n=()=>s.slice(0,4),c=t=>e.categoryLabels[t]||t;export{c as a,a as b,n as g,s as p};
---
+++ assets/projects-BVR5svGw.js
@@ -0,0 +1,1 @@
+import{s as e}from"./index-BA5uzLcW.js";const s=e.projects,a=t=>s.find(r=>r.slug===t),n=()=>s.slice(0,4),c=t=>e.categoryLabels[t]||t;export{c as a,a as b,n as g,s as p};
